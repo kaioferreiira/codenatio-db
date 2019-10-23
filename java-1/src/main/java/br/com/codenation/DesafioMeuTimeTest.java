efb6c99f40c675b4acab73933b90d70dac29c74c
Kaio Ferreira <kaioferreiracosta@hotmail.com>
2019-10-23 13:42:06 -0300

    Inclui jogador e time

--- java-1/src/main/java/br/com/codenation/DesafioMeuTimeTest.java
+++ java-1/src/main/java/br/com/codenation/DesafioMeuTimeTest.java
@@ -6,22 +6,107 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import br.com.codenation.desafio.exceptions.IdentificadorUtilizadoException;
+import br.com.codenation.desafio.exceptions.TimeNaoEncontradoException;
 
 public class DesafioMeuTimeTest {
 
-    private final Map<Long, TimeFutebol> timesCadastradosBD = new HashMap<>();
-    private final Map<Long, Jogador> jogadoresCadastradosBD = new HashMap<>();
+    private static final Map<Long, TimeFutebol> timesCadastradosBD = new HashMap<>();
+    private static final Map<Long, Jogador> jogadoresCadastradosBD = new HashMap<>();
 
 
-	public void incluirTime(Long id, String nome, LocalDate dataCriacao, String corUniformePrincipal, String corUniformeSecundario) {
-		throw new UnsupportedOperationException();
+    public static void carregaBaseDados(){
+        Long idTime = 1l;
+        String nome = "cruzeiro";
+        LocalDate dataCriacao = LocalDate.now();
+        String corUniformePrincipal = "Azul";
+        String corUniformeSecundario = "Branco";
+
+        TimeFutebol timeFutebol1 = new TimeFutebol();
+        timeFutebol1.setId(idTime);
+        timeFutebol1.setNome(nome);
+        timeFutebol1.setDataCriacao(dataCriacao);
+        timeFutebol1.setCorUniformePrincipal(corUniformePrincipal);
+        timeFutebol1.setCorUniformeSecundario(corUniformeSecundario);
+        timesCadastradosBD.put(timeFutebol1.getId(), timeFutebol1);
+
+        Jogador jogador =  new Jogador(1l, 1l, "kaio", LocalDate.now(), 90, new BigDecimal(10000.00));
+        jogadoresCadastradosBD.put(jogador.getId(), jogador);
+
+
+    }
+
+    public static void main(String[] args) {
+        carregaBaseDados();
+        incluirTime(2l, "flamengo", LocalDate.now(), "vermelho", "branco");
+        incluirJogador(2l, 2l, "lucas", LocalDate.now(), 30, new BigDecimal(2000.00));
+        incluirJogador(3l, 1l, "mateus", LocalDate.now(), 60, new BigDecimal(6000.00));
+
+    }
+
+    private static Optional<TimeFutebol> timesFindById(Long idTime) {
+        TimeFutebol timeFutebol = timesCadastradosBD.get(idTime);
+        return Optional.ofNullable(timeFutebol);
+    }
+
+    private static Optional<Jogador> jogadorFindById(Long idjogador) {
+        Jogador jogador = jogadoresCadastradosBD.get(idjogador);
+        return Optional.ofNullable(jogador);
+    }
+
+
+    private static void cadastraNovoTimeBD(TimeFutebol timeFutebol) {
+        timesCadastradosBD.put(timeFutebol.getId(), timeFutebol);
+    }
+
+    private static void salvaJogadorBD(Jogador jogador) {
+        jogadoresCadastradosBD.put(jogador.getId(), jogador);
+    }
+
+//    Realiza a inclusão de um novo time.
+//
+//    Exceções:
+//    Caso o identificador já exista, retornar br.com.codenation.desafio.exceptions.IdentificadorUtilizadoException
+
+	public static void incluirTime(Long id, String nome, LocalDate dataCriacao, String corUniformePrincipal,
+            String corUniformeSecundario) {
+
+        Boolean validaTimeBanco = timesFindById(id).isPresent();
+
+        if (validaTimeBanco.equals(Boolean.TRUE)){
+            throw new IdentificadorUtilizadoException();
+        }
+        TimeFutebol timeFutebol = new TimeFutebol(id,nome, dataCriacao, corUniformePrincipal, corUniformeSecundario, null);
+        cadastraNovoTimeBD(timeFutebol);
 	}
 
-	public void incluirJogador(Long id, Long idTime, String nome, LocalDate dataNascimento, Integer nivelHabilidade, BigDecimal salario) {
-		throw new UnsupportedOperationException();
+//    Realiza a inclusão de um novo jogador.
+//    Exceções:
+//    Caso o identificador já exista, retornar br.com.codenation.desafio.exceptions.IdentificadorUtilizadoException
+//    Caso o time informado não exista, retornar br.com.codenation.desafio.exceptions.TimeNaoEncontradoException
+    public static void incluirJogador(Long id, Long idTime, String nome, LocalDate dataNascimento,
+            Integer nivelHabilidade,
+            BigDecimal salario) {
+
+       Boolean validaTime = timesFindById(idTime).isPresent();
+       Boolean validaJogador = jogadorFindById(id).isPresent();
+
+        if (validaJogador.equals(Boolean.TRUE)){
+            throw new IdentificadorUtilizadoException();
+        }
+
+        if (validaTime.equals(Boolean.FALSE)){
+            throw new TimeNaoEncontradoException();
+        }
+        Jogador jogador = new Jogador(id, idTime, nome, dataNascimento, nivelHabilidade, salario);
+
+        salvaJogadorBD(jogador);
 	}
 
-	public void definirCapitao(Long idJogador) {
+
+    public void definirCapitao(Long idJogador) {
 		throw new UnsupportedOperationException();
 	}
 
--- java-1/src/main/java/br/com/codenation/TimeFutebol.java
+++ java-1/src/main/java/br/com/codenation/TimeFutebol.java
@@ -1,4 +1,85 @@
 package br.com.codenation;
 
+import java.time.LocalDate;
+
 public class TimeFutebol {
+
+    private Long id;
+    private String nome;
+    private LocalDate dataCriacao;
+    private String corUniformePrincipal;
+    private String corUniformeSecundario;
+    private Long idCapitaoTime;
+
+    public TimeFutebol() {
+    }
+
+    public TimeFutebol(Long id, String nome, LocalDate dataCriacao, String corUniformePrincipal, String corUniformeSecundario, Long idCapitaoTime) {
+        this.id = id;
+        this.nome = nome;
+        this.dataCriacao = dataCriacao;
+        this.corUniformePrincipal = corUniformePrincipal;
+        this.corUniformeSecundario = corUniformeSecundario;
+        this.idCapitaoTime = idCapitaoTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getCorUniformePrincipal() {
+        return corUniformePrincipal;
+    }
+
+    public void setCorUniformePrincipal(String corUniformePrincipal) {
+        this.corUniformePrincipal = corUniformePrincipal;
+    }
+
+    public String getCorUniformeSecundario() {
+        return corUniformeSecundario;
+    }
+
+    public void setCorUniformeSecundario(String corUniformeSecundario) {
+        this.corUniformeSecundario = corUniformeSecundario;
+    }
+
+    public Long getIdCapitaoTime() {
+        return idCapitaoTime;
+    }
+
+    public void setIdCapitaoTime(Long idCapitaoTime) {
+        this.idCapitaoTime = idCapitaoTime;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeFutebol{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", dataCriacao=" + dataCriacao +
+                ", corUniformePrincipal='" + corUniformePrincipal + '\'' +
+                ", corUniformeSecundario='" + corUniformeSecundario + '\'' +
+                ", idCapitaoTime=" + idCapitaoTime +
+                '}';
+    }
 }
